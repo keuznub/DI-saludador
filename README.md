03262b4018efc3188c9ee1be2deec1019807af06
Keuznub <breixopastoriza@gmail.com>
2024-09-13 13:41:08 +0200

    Primera version

---
+++ README.md
@@ -0,0 +1,16 @@
+# Saludator
+
+## Descripción
+Aplicacion web que te saluda por pantalla
+
+## Etapas
+
+### Sprint 1
+- [x] crear la estructura html
+- [x] aplicar estilos con css
+- [x] dotar de comportamientos con js
+- [ ] deploy
+
+### Sprint 2
+- [ ] mejorar el rendimiento
+- [ ] colocar conffeti
---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('boton').addEventListener('click', saludar)
+
+function saludar(){
+    const holaSpan = document.getElementById('hola').textContent
+    if(document.getElementById('hola').textContent.includes('hola')){
+        document.getElementById('hola').textContent = "..."    
+    }else{
+        document.getElementById('hola').textContent = "hola"    
+    }
+                
+}
+
+
+
